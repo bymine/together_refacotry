e9f61b210e5420cb014344f03d3f63d92b2b8507
bymine <frfr0723@gmail.com>
2022-05-03 13:48:00 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:together_refacotry/src/bindings/init_binding.dart';
+import 'package:together_refacotry/src/components/splash_screen.dart';
+import 'package:together_refacotry/src/controllers/auth_controller.dart';
+import 'package:together_refacotry/src/pages/login_page.dart';
+import 'package:together_refacotry/src/pages/sign_up_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Together',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          titleTextStyle: TextStyle(color: Colors.black),
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      initialRoute: '/',
+      initialBinding: InitBindings(),
+      getPages: [
+        GetPage(name: '/', page: () => const SplashPage()),
+        GetPage(name: '/login', page: () => const LoginPage()),
+        GetPage(name: '/signUp', page: () => const SignUpPage()),
+        GetPage(name: '/app', page: () => const HomePage()),
+      ],
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("홈"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                AuthController.to.logOut();
+              },
+              icon: Icon(Icons.exit_to_app))
+        ],
+      ),
+    );
+  }
+}
