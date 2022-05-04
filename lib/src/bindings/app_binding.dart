d355ed3c909f0879e854b6488f0a904cbd108815
bymine <frfr0723@gmail.com>
2022-05-04 12:50:00 +0900

    no message

--- lib/src/bindings/app_binding.dart
+++ lib/src/bindings/app_binding.dart
@@ -1,11 +1,21 @@
 import 'package:get/get.dart';
 import 'package:together_refacotry/src/controllers/app_controller.dart';
+<<<<<<< Updated upstream
+=======
+
+import 'package:together_refacotry/src/controllers/home_controller.dart';
+
+>>>>>>> Stashed changes
 import 'package:together_refacotry/src/controllers/calendar_controller.dart';
 
 class AppBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(AppController());
+<<<<<<< Updated upstream
+=======
+    Get.put(HomeController());
+>>>>>>> Stashed changes
     Get.put(CalendarController());
   }
 }
